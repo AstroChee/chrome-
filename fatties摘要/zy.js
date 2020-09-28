79667668179a52f35cb5bee4c0c4e8b910b967f8
AstroChee <astro904338847@163.com>
2020-09-28 19:53:16 +0800

    Create zy.js

---
+++ fatties摘要/zy.js
@@ -0,0 +1,19 @@
+document.getElementById('buttonProcess').onclick = bt;
+function bt(){
+    var text  = document.getElementById("zh").value;
+    var data = {
+      data: JSON.stringify({
+                        "value":text
+                    })
+   };
+    $.ajax({
+        type: 'post',
+        async : false,
+        url: 'http://127.0.0.1:8989/text',
+        data: data,
+        success: function (data) {
+            document.getElementById("jg").hidden = "";
+            document.getElementById("yx").firstChild.data = data;
+        }
+    });
+}
